dd9afbd1d0b1c615875f3d42b13fd0c805a50653
ohrisbraun <ohrisbraun@gmail.com>
2021-09-28 14:40:17 +0300

    fix and install redux and react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import {GameFiledContainer} from "./component/gameFiledContainer";
-import {GameFiled} from "./component/gameFiled";
-import {useSelector} from "react-redux";
-
-
 
 const App: React.FunctionComponent = () => {
-    console.log(useSelector((main) => main)));
     return (<BrowserRouter>
-      {/*<GameFiledContainer />*/}
-        {/*<GameFiled livesPlayerOne={2} livesPlayerTwo={1}/>*/}
+      <GameFiledContainer />
     </BrowserRouter>);
 }
 
